1cc8644ffaca4248b1a1a103a517c75da9d7245c
klimandri11 <andrey.klimenko.99@mail.ru>
2020-08-31 01:34:01 +0300

    Answer and quection model

---
+++ db/migrate/20200830222541_add_reference_question_to_answer.rb
@@ -0,0 +1,5 @@
+class AddReferenceQuestionToAnswer < ActiveRecord::Migration[6.0]
+  def change
+    add_reference(:answers, :question, foreign_key: true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_191045) do
+ActiveRecord::Schema.define(version: 2020_08_30_222541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_08_30_191045) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_08_30_191045) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
