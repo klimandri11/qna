1fb5e2d32b46e7e50295dce3e17263ec6e545b06
klimandri11 <andrey.klimenko.99@mail.ru>
2020-09-01 05:51:33 +0300

    Question controller with spec (path #update, delete #destroy)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -17,12 +17,25 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-    
+
     if @question.save
       redirect_to @question
     else
       render :new
     end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
