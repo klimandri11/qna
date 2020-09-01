a3b8177c0d4f08044259e9f1921225dd20cffd42
klimandri11 <andrey.klimenko.99@mail.ru>
2020-09-01 07:15:38 +0300

    add helper_method answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,6 +14,12 @@ class AnswersController < ApplicationController
 
   private
 
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  helper_method :answer
+
   def answer_params
     params.require(:answer).permit(:body)
   end
