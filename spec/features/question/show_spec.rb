40d63d11bbbb8ba2a4a8a512da2598fbc89067d6
klimandri11 <andrey.klimenko.99@mail.ru>
2020-09-05 08:20:15 +0300

    Add feature question show spec

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can browse the question and its answers', %q{
+  To find the answer to the question
+  I'd like to view a list of answers to the it
+} do
+
+  given(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given!(:answers) { create_list(:answer, 4, question: question) }
+
+  scenario 'User tries browse the question and its answers' do
+    visit questions_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+end
